96eaf42c3d1b7b939b067f01e9c0faddd8872b4a
Aaron <ronjohn90z@gmail.com>
2024-09-15 13:24:02 -0400

    html bug fix, screen fix

--- js/index.js
+++ js/index.js
@@ -1,3 +1,25 @@
+// WINDOW
+function setViewportHeight() {
+  // Get the viewport height and multiply it by 1% to get a value for 1vh unit
+  let vh = window.innerHeight;// * 0.01;
+  let vw = window.innerWidth;
+  console.log(vh,vw)
+  
+  // Set the value in the custom property --vh
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+  document.documentElement.style.setProperty('--fvh', `${vh}px`);
+  document.documentElement.style.setProperty('--fvw', `${vw}px`);
+}
+
+// Set the height on page load
+// setViewportHeight();
+setTimeout(setViewportHeight, 3000);
+
+
+// Update the height on window resize and orientation change
+window.addEventListener('resize', setViewportHeight);
+window.addEventListener('orientationchange', setViewportHeight);
+
 // NAVBAR ANIMATIONS
 const navbar = document.getElementById("navbar");
 const overlay = document.getElementById("navOverlay");
@@ -23,7 +45,7 @@ overlayCloseBtn.addEventListener("click", toggleNav);
 // SECTION ANIMATIONS
 const viewportHeight = window.innerHeight;
 const sections = document.querySelectorAll(".section-container");
-let currentSectionIndex = 0, previousSectionIndex = 0; // Keeps track of the currently active section
+let currentSectionIndex = 0; // Keeps track of the currently active section
 
 function animateSection(section, progress) {
   // progress is a value between 0 and 1
@@ -41,13 +63,21 @@ function broadcastSectionChange(previousIndex, newIndex) {
   window.dispatchEvent(event);
 }
 
+function between(n, low=0, high=1){
+  return (n > low && n < high);
+}
+
 function handleSectionScroll() {
   let scrollY = window.scrollY;
   let scrollProgress = scrollY / viewportHeight;
+  // let jump = currentSectionIndex + .90;
+  // if(between(scrollProgress, jump, currentSectionIndex + 1)){
+  //   scrollProgress += .10;
+  // }
+
   const newIndex = Math.floor(scrollProgress);
   if (currentSectionIndex != newIndex) {
     broadcastSectionChange(currentSectionIndex, newIndex);
-    previousSectionIndex = currentSectionIndex;
     currentSectionIndex = newIndex;
   }
 
@@ -60,7 +90,7 @@ function handleSectionScroll() {
   sections.forEach((section, i) => {
     const rect = section.getBoundingClientRect();
     const relativePosition = rect.top / viewportHeight; // relative position in the viewport
-    const progress = Math.abs(currentSectionIndex - scrollProgress);
+    let progress = Math.abs(currentSectionIndex - scrollProgress);
     const isLastSection = currentSectionIndex == sections.length - 1;
     if (i === currentSectionIndex && !isLastSection) {
       // Section is currently in view
